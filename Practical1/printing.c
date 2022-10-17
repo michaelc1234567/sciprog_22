b441c2f5e57fc468ee5022bda8d4574dd70eb863
Michael_Connolly_Brightspace <115847248+michaelc1234567@users.noreply.github.com>
2022-10-17 22:22:35 +0100

    Practical 1
    Upload of practical 1's printing and scanning code. MC

---
+++ Practical1/printing.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) {
+   int accountnum=1;
+   float subtotal=1234.56, total=7890.12;
+
+   printf("Account: %d Subtotal: %.2f Total: %.2f\n",accountnum,
+        subtotal,total);
+
+  return 0;
+}
+
+
+
